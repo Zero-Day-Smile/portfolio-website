c343fcca793a9234adb13df877fb6ac929c8553f
Amish Kumar <91964710+Zero-Day-Smile@users.noreply.github.com>
2025-02-18 22:53:43 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,6 @@
-// Initialize EmailJS with your user ID
-(function() {
-  emailjs.init("service_ak");
-})();
-
-// Handle form submission
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevent default form submission
-
-  // Send the form data via EmailJS
-  emailjs.sendForm('service_ak', 'template_ak', this)
-    .then(function(response) {
-      alert("Message sent successfully!");
-    }, function(error) {
-      alert("Error: " + JSON.stringify(error));
-    });
-});
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
